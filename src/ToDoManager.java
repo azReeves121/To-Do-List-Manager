b26d7319c40e29144e07f91f9d414c3fc7604832
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2025-02-27 18:59:54 -0330

    ToDoManager.java complete

--- src/ToDoManager.java
+++ src/ToDoManager.java
@@ -1,2 +1,55 @@
 public class ToDoManager {
+    private User[] users;
+    private int userCount;
+    private final int MAX_USERS = 10; // Max limit for simplicity
+
+    public ToDoManager() {
+        users = new User[MAX_USERS];
+        userCount = 0;
+    }
+
+    public boolean addUser(String name) {
+        if (userCount >= MAX_USERS) {
+            System.out.println("User limit reached!");
+            return false;
+        }
+        if (getUser(name) != null) {
+            System.out.println("User already exists!");
+            return false;
+        }
+        users[userCount++] = new User(name);
+        return true;
+    }
+
+    private User getUser(String name) {
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].getName().equalsIgnoreCase(name)) {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean addTaskToUser(String userName, String taskDescription) {
+        User user = getUser(userName);
+        if (user != null) {
+            user.addTask(taskDescription);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean markTaskAsCompleted(String userName, String taskDescription) {
+        User user = getUser(userName);
+        return user != null && user.markTaskAsCompleted(taskDescription);
+    }
+
+    public void showUserTasks(String userName) {
+        User user = getUser(userName);
+        if (user != null) {
+            user.showTasks();
+        } else {
+            System.out.println("User not found!");
+        }
+    }
 }
