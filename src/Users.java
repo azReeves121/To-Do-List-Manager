83552d42309dc8cbf456ac2b28fab7bad35e2c2e
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2025-02-27 12:04:06 -0330

    Users.java complete

--- src/Users.java
+++ src/Users.java
@@ -1,2 +1,26 @@
 public class Users {
+    private String name;
+    private TaskList taskList;
+
+    public Users(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTask(String description) {
+        taskList.addTask(description);
+    }
+
+    public boolean markTaskAsCompleted(String description) {
+        return taskList.markTaskAsCompleted(description);
+    }
+
+    public void showTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
+    }
 }
